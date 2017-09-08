6cbdd0312333e51c238b3feb567052f8cd31652c
monster1935 <946815480@qq.com>
2017-09-08 17:54:09 +0800

    add categories,tags,about page and the those in sidebar

--- source/js/src/index.js
+++ source/js/src/index.js
@@ -1,7 +1,7 @@
-(function(){
-    // toggle按钮点击处理函数
-    $('a.btn-more').click(function(){
-        $('body').toggleClass('side');
-        $('.sidebar').toggle();
-    });
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?8ef16df5364dcc2746503f1fbda70bab";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
 })();
